3bcdd1ea871ce3bd16db06e7833dc0ee185cee6b
root <root@localhost.localdomain>
2025-02-23 20:05:12 -0500

    Created layout, homepage and styles

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "@/assets/styles/globals.css"
+import { title } from "process";
 
-const MainLayout = () => {
-  return <div>MainLayout</div>;
+export const metadata = {
+  title: "PropertyPulse | Find the Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties"
+};
+
+const MainLayout = ({ children }) => {
+  return (
+  <html lang="en">
+    <body>
+      <div>{children}</div>
+    </body>
+  </html>
+  )
 };
 
 export default MainLayout;
