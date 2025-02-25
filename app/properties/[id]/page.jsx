586c1591260bfd5063bb7fb7443d487c61f95a81
josue <josuerushanika@gmail.com>
2025-02-25 13:22:04 -0500

    Add clients components

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client";
+
 
 const PropertyPage = () => {
-  return (
-    <div>PropertyPage</div>
-  )
-}
 
-export default PropertyPage
+  return (
+    <div>
+      <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
+        Go Home {Pathname}
+      </button>
+    </div>
+  );
+};
+
+export default PropertyPage;
