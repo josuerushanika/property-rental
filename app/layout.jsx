33f4f501172821d2cf08dcfa35e359a0a3e92731
josue <josuerushanika@gmail.com>
2025-02-25 15:54:41 -0500

    Add navbar and some logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Navbar from "../components/Navbar"
 import "../assets/styles/globals.css"
 
 export const metadata = {
@@ -11,7 +11,8 @@ const MainLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div>{children}</div>
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   )
