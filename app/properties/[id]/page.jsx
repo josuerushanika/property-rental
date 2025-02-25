c63928c2a59bcf3c55f8caeaba91dabe026684cb
josue <josuerushanika@gmail.com>
2025-02-25 14:58:53 -0500

    Update files

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -5,9 +5,7 @@ const PropertyPage = () => {
 
   return (
     <div>
-      <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
-        Go Home {Pathname}
-      </button>
+      PropertyPage
     </div>
   );
 };
