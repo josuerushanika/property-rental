d6fb360ee31209ba9f55f530f87db8f92619f6fc
josue <josuerushanika@gmail.com>
2025-02-28 15:30:28 -0500

    Add login restriction

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-
 
 ## Getting Started
 
-<!-- Left coding  -->
+<!-- Left coding 12 active links 6 minu  -->
 
 First, run the development server:
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,6 +11,7 @@ import { FaGoogle } from "react-icons/fa";
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const pathname = usePathname();
 
@@ -211,14 +212,17 @@ const Navbar = () => {
             >
               Properties
             </Link>
-            <Link
-              href="/properties/add"
-              className={`${
-                pathname === "/properties/add" ? "bg-black" : ""
-              } text-white block rounded-md px-3 py-2 text-base font-medium`}
-            >
-              Add Property
-            </Link>
+            {isLoggedIn && (
+              <Link
+                href="/properties/add"
+                className={`${
+                  pathname === "/properties/add" ? "bg-black" : ""
+                } text-white block rounded-md px-3 py-2 text-base font-medium`}
+              >
+                Add Property
+              </Link>
+            )}
+
             <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
               <FaGoogle className="text-white " />
               <span>Login or Register</span>
