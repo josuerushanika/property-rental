43285eb8a7577b93d9e31cb2bd41bf9315f4523d
josue <josuerushanika@gmail.com>
2025-02-28 19:39:02 -0500

    Add footer component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,11 @@
-import Navbar from "../components/Navbar"
-import "../assets/styles/globals.css"
-
+import Navbar from "../components/Navbar";
+import "../assets/styles/globals.css";
+import "../components/Footer";
+import Footer from "../components/Footer";
 export const metadata = {
   title: "PropertyPulse | Find the Perfect Rental",
   description: "Find your dream rental property",
-  keywords: "rental, find rentals, find properties"
+  keywords: "rental, find rentals, find properties",
 };
 
 const MainLayout = ({ children }) => {
@@ -13,9 +14,10 @@ const MainLayout = ({ children }) => {
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
-  )
+  );
 };
 
 export default MainLayout;
