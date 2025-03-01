efadebc0d2405c4bf7e92611273c63c97dc08e03
josue <josuerushanika@gmail.com>
2025-03-01 08:09:13 -0500

    Fetch data from json

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,23 @@
-import properties from "@/properties.json";
+import properties from "./properties.json";
 
 const PropertiesPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <p> No Property Found</p>
+          ) : (
+            <div className="container-xl lg:container m-auto px-4 py-6">
+              {properties.map((property) => (
+                <div> {property.name}</div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default PropertiesPage;
