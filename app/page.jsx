d1a0ae00457f8259aa0cd6cce2ea93fa83089972
josue <josuerushanika@gmail.com>
2025-02-28 19:13:44 -0500

    Add components

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Link from "next/link";
 import Hero from "../components/Hero";
+import InfoBoxes from "../components/InfoBoxes";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <InfoBoxes />
     </>
   );
 };
