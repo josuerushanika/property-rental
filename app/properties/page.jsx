5f02eb6ee2843b1972afe82a686ddf089a667d24
josue <josuerushanika@gmail.com>
2025-03-02 09:17:49 -0500

    Add fetchProperties

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,19 +1,37 @@
-import properties from "../../properties.json";
 import PropertyCard from "../../components/PropertyCard";
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching properties:", error);
+    return []; // Return an empty array on error
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+
+  //Sort properties by date
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
   return (
     <div>
       <section className="px-4 py-6">
         <div className="container-xl lg:container m-auto px-4 py-6">
-          {properties.length === 0 ? (
-            <p> No Property Found</p>
-          ) : (
-            <div className="container-xl lg:container m-auto px-4 py-6">
+          {properties.length > 0 ? (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
               {properties.map((property) => (
                 <PropertyCard key={property._id} property={property} />
               ))}
             </div>
+          ) : (
+            <p className="text-center text-gray-500">No Property Found</p>
           )}
         </div>
       </section>
