4103a26fb59c27d58ca571b7bfba887a26bb0ab8
josue <josuerushanika@gmail.com>
2025-03-02 17:37:46 -0500

    Fetch single property

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,7 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+//Fetch all properties
+
 async function fetchProperties() {
     try {
         // Handle the case where the domain is not available yet
@@ -18,4 +20,25 @@ async function fetchProperties() {
     }
   }
 
-  export { fetchProperties }
+  // Fetch single property
+
+  async function fetchProperty(id) {
+    try {
+        // Handle the case where the domain is not available yet
+        if (!apiDomain) {
+            return null;
+        }
+      const res = await fetch(`${apiDomain}/properties/${id}`);
+  
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      return await res.json();
+    } catch (error) {
+      console.error("Error fetching properties:", error);
+      return null; // Return an empty array on error
+    }
+  }
+
+
+  export { fetchProperties, fetchProperty }
