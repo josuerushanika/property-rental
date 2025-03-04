88ac56892dd33f4fbd64d6b0c467fd0964cfd1d4
josue rushanika <100031798+josuerushanika@users.noreply.github.com>
2025-03-04 02:58:23 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,24 @@
 
+import Navbar from "../components/Navbar";
+import "../assets/styles/globals.css";
+import "../components/Footer";
+import Footer from "../components/Footer";
+export const metadata = {
+  title: "PropertyPulse | Find the Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
