b26ea9d7b44f7e95065c25575f938fa2e3b1e433
josue rushanika <100031798+josuerushanika@users.noreply.github.com>
2025-05-16 19:10:11 +0200

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
 import logo from "../assets/images/logo.png";
-
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-
   return (
     <footer className="bg-gray-200 py-4 mt-24">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
