b4d3af0afbe818116aa48723129ad06775ccbbaa
josuerushanika <josuerushanika@gmail.com>
2025-05-18 23:21:14 +0200

    Refactor some statement

--- config/database.js
+++ config/database.js
@@ -5,6 +5,12 @@ let connected = false;
 const connectDB = async () => {
   mongoose.set("strictQuery", true);
 
+  // If the database is already connected, don't connect again
+  if (connected) {
+    console.log("MongoDB is already connected...");
+    return;
+  }
+
   // Connect to MongoDB
   try {
     await mongoose.connect(process.env.MONGODB_URI);
